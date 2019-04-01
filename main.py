cdfb85ee96880794e3816a0e5c80118f6baadd8a
gmc242 <32424615+gmc242@users.noreply.github.com>
2019-04-01 08:01:44 -0600

    Reescritura de función bisiesto
    Refactorización de código según álgebra lógica vista en https://es.wikipedia.org/wiki/A%C3%B1o_bisiesto

--- main.py
+++ main.py
@@ -6,33 +6,31 @@ def main():
 #R0 se encarga de verificar si es una tupla de 3
 def R0(fecha):
 
-    partes= fecha.split(",")                            #Separamos la fecha por comas, haciendo un esplit
-    if len(partes) != 3:                                #Si el split es diferente de 3 no es una fecha completa o pusieron mas datos
-        print("No es tupla")
-        return False
+    if(type(fecha) == tuple and len(fecha) == 3):
+        return True
+    return False
 
 
-    for item in partes:
-        if(int(item) < 0):                              #Tomamos cada elemento de la fecha(año,mes,dia) y vemos si es positivo
-            print("Deben ser numeros positvos")
-            return False
-    print("Si es tupla")
-    return True
+#R0((2015,4,2))
 
-#R0("2015,2,4")
+
 def R1(bisiesto):                                       #https://es.wikihow.com/calcular-los-a%C3%B1os-bisiestos
 
-    if( bisiesto % 4 == 0):                             #Se Tiene que ver si es divisible entre 4, si lo es sigue a otra prueba, sino no lo es
-
-        if(bisiesto % 100 != 0):                        #Se revisa si es divisible entre 100, si si es divisible, se hace otra prueba, sino ya demuestra ser bisiesto
-            return True
-
-            if(bisiesto % 400 == 0):                    #La ultima prueba es probar si es divisible entre 400, si lo es, es bisiesto
-                return True
-    else:
-        return False
+    return (bisiesto % 4 == 0) and ((bisiesto % 100 != 0) or (bisiesto % 400 == 0))
 
 #R1(2304)
 
 
+def R2(fecha):
+    if len(fecha) != 3:                                #Si el split es diferente de 3 no es una fecha completa o pusieron mas datos
+        print("No es tupla de 3")
+        return False
 
+
+    for item in fecha:
+        if(int(item) < 0):                              #Tomamos cada elemento de la fecha(año,mes,dia) y vemos si es positivo
+            print("Deben ser numeros positvos")
+            return False
+    return True
+
+#print(R2((2015,5,5)))
